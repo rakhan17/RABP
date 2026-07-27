83b15854d463fd545a74582e9947c282ab837766
Rakhan Atthaya <rakhanatanox@gmail.com>
2026-07-27 08:48:50 +0800

    first commit

---
+++ src/firebase.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDC87zcVFkhL3xvGV8_ArXgLHa4OdYO7P0",
+  authDomain: "rabp-473e4.firebaseapp.com",
+  projectId: "rabp-473e4",
+  storageBucket: "rabp-473e4.firebasestorage.app",
+  messagingSenderId: "450440073157",
+  appId: "1:450440073157:web:54b3ea843eaa65116085ac",
+  measurementId: "G-BBLBH24W1R"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Export instances to use in other components
+export { app, analytics };
